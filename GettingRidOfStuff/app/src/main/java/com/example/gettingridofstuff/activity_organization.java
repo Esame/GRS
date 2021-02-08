b4197f05d510a75e56f3c5d935509207f3541677
EleanorHayes <hayese20@wwu.edu>
2021-02-08 13:53:21 -0800

    Created new java file for organization and made basic function

---
+++ GettingRidOfStuff/app/src/main/java/com/example/gettingridofstuff/activity_organization.java
@@ -0,0 +1,30 @@
+package com.example.gettingridofstuff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class activity_organization extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.organization);
+    }
+
+    public void home(View view){
+
+    }
+
+    public void organization(View view){
+
+    }
+
+    public void inventory(View view){
+
+    }
+}
