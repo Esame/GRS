f49347b0359d06ada27265b1ffe826145e9e5e25
EleanorHayes <hayese20@wwu.edu>
2021-02-08 14:22:12 -0800

    Added basic functions to inventory java file

--- GettingRidOfStuff/app/src/main/java/com/example/gettingridofstuff/activity_inventory.java
+++ GettingRidOfStuff/app/src/main/java/com/example/gettingridofstuff/activity_inventory.java
@@ -1,8 +1,11 @@
 package com.example.gettingridofstuff;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class activity_inventory extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class activity_inventory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+    }
+    public void home(View view){
+
+    }
+
+    public void organization(View view){
+
+    }
+
+    public void inventory(View view){
+
     }
 }
