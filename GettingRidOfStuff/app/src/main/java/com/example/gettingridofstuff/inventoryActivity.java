dce235fecd142b139fd063f951c29ca64bc41696
EleanorHayes <hayese20@wwu.edu>
2021-02-08 18:30:42 -0800

    Updated inventoryActivity to handle buttons from header, although buttons have not yet been created in the header

--- GettingRidOfStuff/app/src/main/java/com/example/gettingridofstuff/inventoryActivity.java
+++ GettingRidOfStuff/app/src/main/java/com/example/gettingridofstuff/inventoryActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -13,17 +14,47 @@ public class inventoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+        activity_organization.ButtonHandler bh = new activity_organization.ButtonHandler();
+        Button home_button = (Button) findViewById(R.id.homebutton);
+        homebutton.setOnClickListener(bh);
+        Button organization_button = (Button) findViewById(R.id.organizationbutton);
+        organizationbutton.setOnClickListener(bh);
+        Button inventory_button = (Button) findViewById(R.id.inventorybutton);
+        inventorybutton.setOnClickListener(bh);
+
     }
     public void home(View view){
+        this.finish();
+        //animation should come from right to left because inventory is farthest right
+        //overridePendingTransition(to left enter, to left exit);
 
 
     }
 
     public void organization(View view){
-
+        this.finish();
+        //animation should come from right to left because inventory is farthest right
+        //overridePendingTransition(to left enter, to left exit);
     }
 
     public void inventory(View view){
+        //should do nothing because it is the current screen
+        return;
+    }
+    private class ButtonHandler implements View.OnClickListener{
 
+        public void onClick(View v){
+            int buttonClicked = v.getId();
+            if(buttonClicked == findViewById(R.id.homebutton)){
+                home(v);
+            }
+            else if(buttonClicked == findViewById(R.id.organization)){
+                organization(v);
+            }
+            else if(buttonClicked == findViewById(R.id.inventorybutton)){
+                inventory(v);
+            }
+
+        }
     }
 }
