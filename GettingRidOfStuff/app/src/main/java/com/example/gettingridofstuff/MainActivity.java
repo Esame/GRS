dadb107d4215d7988ff2f15db0e5180b31f7099d
aryawhitehorn <whiteha4@wwu.edu>
2021-02-17 22:07:57 -0800

    second half of charities done

--- GettingRidOfStuff/app/src/main/java/com/example/gettingridofstuff/MainActivity.java
+++ GettingRidOfStuff/app/src/main/java/com/example/gettingridofstuff/MainActivity.java
@@ -70,7 +70,13 @@ public class MainActivity extends AppCompatActivity {
         Charity charity6 = new Charity("Bellingham Food Bank", "3pm - 6pm Wed, 1pm - 4pm Tues and Fri", "0", "Food", "1824 Ellis St, Bellingham, WA 98225");
         Charity charity7 = new Charity("Worn Again Thrift", "11am - 6pm Tues - Sun", "0", "Clothing", "232 E Champion St, Bellingham, WA 98225");
         Charity charity8 = new Charity("The RE Store", "11am - 5pm, Tues - Sat", "0", "Tools, Building materials, House care supplies", "2309 Meridian Street, Bellingham, WA 98225");
-
+        Charity charity9 = new Charity("We Care of Whatcom County", "10am-3pm Sat - Tues", "0", "Clothing",  "3550 Meridian St Ste 2, Bellingham, WA 98225");
+        Charity charity10 = new Charity("C4PIN\n" + "(Computers for people in need)", "9am-1pm Mon - Fri", "0", "Electronics", "2004 James St, Bellingham, WA 98225");
+        Charity charity11 = new Charity("Value Village"," 11am - 7pm everyday","0", "Clothing, Homegoods, Electronics", "150 E Bellis Fair Pkwy, Bellingham, WA 98226");
+        Charity charity12 = new Charity("Habitat for Humanity in Whatcom County","9am - 6pm Tues-Fri\n" + "10am - 5pm Sat", "0", "Clothing, Homegoods, Electronics", "1825 Cornwall Ave, Bellingham, WA 98225");
+        Charity charity13 = new Charity("Lydia Place", "9am - 5pm Mon-Fri", "0", "Clothing, Homegoods, Electronics, Vehicles, Tools, House care supplies", "1701 Gladstone St, Bellingham, WA 98229");
+        Charity charity14 = new Charity("Trash to Treasures Thrift Store", "10am - 7pm Weds - Fri, 9 am - 7pm Sat, 9 am - 7pm Sun", "0", "Clothing", "436 W Bakerview Rd STE 112, Bellingham, WA 98226");
+        Charity charity15 = new Charity("Labels Women’s Consignment", "10am - 6pm Wed - Sat\n" + "11am - 5pm Sun", "0","Clothing, Homegoods","2332 James St, Bellingham, WA 98225");
         db.insert(charity1);
         db.insert(charity2);
         db.insert(charity3);
@@ -79,6 +85,13 @@ public class MainActivity extends AppCompatActivity {
         db.insert(charity6);
         db.insert(charity7);
         db.insert(charity8);
+        db.insert(charity9);
+        db.insert(charity10);
+        db.insert(charity12);
+        db.insert(charity13);
+        db.insert(charity14);
+        db.insert(charity15);
+
     }
     /*ButtonHandler function is used to listen to buttons in the header and change their functionality*/
     private class ButtonHandler implements View.OnClickListener {
